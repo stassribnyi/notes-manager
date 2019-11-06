6aa4b50d067c406c96bd24c7baca5aa205e337e3
Stas Sribnyi <stas.sribnyi@gmail.com>
2019-11-06 13:29:52 +0000

    Add chulk to print colorful messages in console

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+const validator = require('validator');
+const chalk = require('chalk');
+
 const notes = require('./notes');
 
-console.log(notes.getNotes());
+const { log } = console;
+
+log(chalk.bgWhiteBright(notes.getNotes()));
+log(chalk.green(validator.isEmail('stas@sribnyi.com')));
+log(chalk.red(validator.isEmail('stas.sribnyi.com')));
+log(chalk.blue(validator.isURL('https://test.io')));
+log(chalk.magenta(validator.isURL('https://testio')));
