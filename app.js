5d4f16722c22a7bbd2e06d30c350b0699188e93e
Stas Sribnyi <stas.sribnyi@gmail.com>
2019-11-06 11:39:13 +0000

    Add notes.txt to .gitignore and cleanup unrelated to notes functionality codebase

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-const utils = require('./utils');
 const notes = require('./notes');
 
-const a = 235.2345;
-const b = 432.231;
-const c = utils.add(a, b);
-
-console.log(`Hi, ${utils.name}`);
-console.log(`${a} + ${b} = ${c}`);
-console.log(`Getting your notes...`);
 console.log(notes.getNotes());
--- utils.js
+++
@@ -1,8 +0,0 @@
-const name = 'Stas';
-
-const add = (x, y) => x + y;
-
-module.exports = {
-  name,
-  add
-};
