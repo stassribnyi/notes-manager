b27ab4266dae853c7f9b47383d2f401baa916c1f
Stas Sribnyi <stas.sribnyi@gmail.com>
2019-11-06 11:19:29 +0000

    Update file via appendFileSync

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 const fs = require('fs');
 
-fs.writeFileSync('notes.txt', 'This file was created by me from Node.JS');
+// fs.writeFileSync('notes.txt', 'This file was created by me from Node.JS. Just updated it with additional text!');
+
+fs.appendFileSync('notes.txt', '\nAppended text here!!!');
