4115f933813339785da375d928ddd3b8585b837f
Stas Sribnyi <stas.sribnyi@gmail.com>
2019-11-06 15:11:21 +0000

    Extend 'add' command

--- app.js
+++ app.js
@@ -5,27 +5,66 @@ const notes = require('./notes');
 
 const { log } = console;
 
-const addCommand = (command, describe, handler) =>
+const addCommand = (command, describe, handler, builder = {}) =>
   yargs.command({
     command,
     describe,
-    handler
+    handler,
+    builder
   });
 
-addCommand('add', 'Add a new note', () => {
-  log('Adding a new note!');
-});
+addCommand(
+  'add',
+  'Add a new note',
+  ({ title, body }) => {
+    log(`Adding a new note...\nTitle: ${title}\nBody: ${body}`);
+  },
+  {
+    title: {
+      describe: 'A note title',
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      describe: 'A note body',
+      demandOption: true,
+      type: 'string'
+    }
+  }
+);
 
-addCommand('remove', 'Remove a note', () => {
-  log('Removing a note!');
-});
+addCommand(
+  'remove',
+  'Remove a note',
+  () => {
+    log('Removing a note!');
+  },
+  {
+    id: {
+      describe: 'An id of a note',
+      demandOption: true,
+      type: 'number'
+    }
+  }
+);
 
 addCommand('list', 'List all notes', () => {
   log('Listing up all notes!');
 });
 
-addCommand('read', 'Read a specific note', ({ id }) => {
-  log(`Reading a note with id: ${id}!`);
-});
+addCommand(
+  'read',
+  'Read a specific note',
+  ({ id }) => {
+    log(`Reading a note with id: ${id}!`);
+  },
+  {
+    id: {
+      describe: 'An id of a note',
+      demandOption: true,
+      type: 'number'
+    }
+  }
+);
 
-log(yargs.argv);
+yargs.parse();
